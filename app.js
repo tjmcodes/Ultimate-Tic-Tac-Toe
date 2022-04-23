9c73a85a7fca877541e0008e49a5b12620fb77d8
Teresa Morini <tjmcodes@gmail.com>
2022-04-23 10:52:30 +0100

    highlight next board  working

--- app.js
+++ app.js
@@ -2,12 +2,9 @@
 // SPLASH SCREEN
 const splash = document.querySelector('.splash');
 
-
 splash.addEventListener('click', () => {
   splash.classList.add('display-none');
 })
-
-
 
 
 // GAME VARIABLES
@@ -19,21 +16,31 @@ const cellCount = width * width
 // HIGHLIGHT BOARD WON
 
 // AUDIO MENU OPTIONS
-const audioElem = document.querySelector('audio')
+const audioMusic = document.querySelector('#music')
 const musicButton = document.querySelector('.musicButton')
-audioElem.loop = true
+audioMusic.muted = true
+audioMusic.loop = true
+audioMusic.src = './sounds/Alpha.wav'
 
-musicButton.addEventListener('click', (event) => playPauseMusic(event, audioElem))
+let muted = true
 
-
-function playPauseMusic(event, audioElem) {
-  audioElem.src = 'sounds/Intense.wav'
-  if (audioElem.pause()) {
-    audioElem.play()
-  } else {
-    audioElem.pause()
+function musicToggle() {
+  muted = !muted
+  if (muted) {
+    audioMusic.muted = true
+    musicButton.textContent = 'UNMUTE'
+  } else if (!muted) {
+    audioMusic.muted = false
+    audioMusic.play()
+    audioMusic.loop = true
+    musicButton.textContent = "MUTE"
   }
-}
+ }
+
+musicButton.addEventListener('click', musicToggle)
+musicButton.textContent = 'UNMUTE'
+
+
 
 // GAME VARIABLES
 let playerTurn = ('X')
@@ -47,15 +54,15 @@ const swapTurns = function() {
   }
 };
 
-let disabledBoard = []
+const disabledBoard = []
 
 
 
-let gridArray1 = []
-let gridArray2 = []
+const gridArray1 = []
+const gridArray2 = []
 
-let playerArray1 = [[],[],[],[],[],[],[],[],[]]
-let playerArray2 = [[],[],[],[],[],[],[],[],[]]
+const playerArray1 = [[],[],[],[],[],[],[],[],[]]
+const playerArray2 = [[],[],[],[],[],[],[],[],[]]
 
 
 let lastTurn = 0
@@ -67,22 +74,22 @@ let gameStart = true
 // GAMESTART
 
  
-function clearBoard(event, cell) {
-  window.location.reload()
+function clearBoard() {
+  setTimeout(() => window.location.reload(), 50)
 }
 // console.log(clearBoard())
 
 // WINNING COMBINATIONS
 
 const winningCombinations = [
-  [1,2,3],
-  [4,5,6],
-  [7,8,9],
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
   [1,4,7],
   [2,5,8],
-  [3,6,9],
-  [1,5,9],
-  [3,5,7]
+  [0,4,8],
+  [2,4,6]
 ];
 
 
@@ -94,11 +101,12 @@ const checkIfWin = playerArray =>
     possibility.every(index => playerArray.includes(index))
   ).length > 0;
 
-console.log(disabledBoard)
+// console.log(disabledBoard)
 
 // Reversing the alert function
 let alerted = false
 
+const splashWin = document.querySelector('.splashWin.none') 
 
 function checkBoardWin(gridsToCheck) { // Parameter name of gridsToCheck
   winningCombinations.map(combination => { // Returns new array of each combination of winningCombinations 
@@ -109,7 +117,7 @@ function checkBoardWin(gridsToCheck) { // Parameter name of gridsToCheck
     })
     if (!alerted && gridsToCheck.length === 3) {
       alerted = true
-      setTimeout(() => alert("GAME WON"),50)
+      setTimeout(() => splashWin.classList.remove('none'),5000)
       clearBoard()
     } else {
       console.log("no win")
@@ -119,24 +127,15 @@ function checkBoardWin(gridsToCheck) { // Parameter name of gridsToCheck
     }
   })
 }
-// DISPLAY
-// const modal = document.querySelector('#myModal')
-// const closeMessage = document.querySelector(".close")
-// function highlightNextBoard() {
-//   return (
-//     (lastTurn === parseInt(parent).classList.add('highlight'))
-//   )
-// }
+
+// SCORES
+// const playerScores = JSON.parse(localStorage.getItem('scores')) || [] // trick to start with empty array if there's no scores.
+const scoresListX = document.querySelector('.player-1')
+const scoresListO = document.querySelector('.player-2')
 
 
 
-// OPEN BOARD OPTION (CLICK)
-function clickAnywhere(parent, clickCell) {
-  return ( 
-    (clickCell === 0) &&
-    ((gameStart) || lastTurn === parseInt(parent))
-  )
-}
+
 
 // GAME SET UP / CLICK EVENT TO PLAY
 
@@ -144,8 +143,9 @@ function createGrid() {
   // creating an element div and adding a class called 'bigBoard' with 9 grids
   for (let index = 0; index < cellCount; index++) {
     const grids = document.createElement('div')
-    grids.classList.add('grids')
-    grids.setAttribute('id', index + 1)
+    // ! Adds a unique class to the grid to identify it.
+    grids.classList.add(`grids-${index}`)
+    grids.setAttribute('id', index)
     // console.log(grids)
   
     // creating an element div (inside the bigBoard div loop) and adding a class called 'innerboard' with 9 cell
@@ -154,9 +154,9 @@ function createGrid() {
       // add class to style
       cell.classList.add('cells')
       // add id #
-      cell.setAttribute('id', index + 1)
+      cell.setAttribute('id', index)
       // console.log(cell)
-  
+      
       // Add the cells
       grids.appendChild(cell)
   
@@ -174,9 +174,21 @@ function createGrid() {
 }
   
 createGrid()
+
+
+
+// OPEN BOARD OPTION (CLICK)
+function clickAnywhere(parent, clickCell) {
+  return ( 
+    (clickCell === 0) &&
+    ((gameStart) || lastTurn === parseInt(parent))
+  )
+}
   
 // GAME PLAY (The click event passes through events and cell as argument and is called here)
-function playGame(event, cell) {              
+function playGame(event, cell) {   
+  // console.log(event)           
+  // console.log(cell.id)           
   // if no winner 
   // console.log(event.target.id)
   
@@ -188,31 +200,41 @@ function playGame(event, cell) {
   // If game has started ignore lastTurn
   const parentElement = cell.parentElement
   const gridId = cell.parentElement.id
+  console.log(gridId)
   const cellId = cell.id
   const clickCell = cell.innerHTML.length
-  // let clickSymbol = cell.innerHTML
- 
+  const nextBoard = document.querySelector(`.grids-${lastTurn}`)
+  const splashWinMini = document.querySelector('.splashWinMini.none') 
+
+  console.log(nextBoard)
   // console.log(!disabledBoard.includes(parseInt(parent)), gridId)
   if ((clickAnywhere(gridId, clickCell)) && (!disabledBoard.includes(parseInt(gridId)))) {
     console.log(playerTurn)
     cell.innerHTML = playerTurn
     gameStart = false
+    console.log(lastTurn)
     
+    // ! Added this code to clear the previous grid with coloured background.
+    if (!gameStart) {
+      console.log('hello')
+      document.querySelector(`.grids-${lastTurn}`).style.backgroundColor = 'transparent'
+    }
     
     if (cell.innerHTML === ('X')) {
       playerArray1[gridId].push(parseInt(cellId))
-
-
+ 
       // If winner:
-      console.log(playerArray1)
+      // console.log(playerArray1)
       if (checkIfWin(playerArray1[gridId]) === true) {
         gridArray1.push(parseInt(gridId))
         checkBoardWin(gridArray1) === true
         disabledBoard.push(parseInt(gridId))
         parentElement.classList.add('highlight-blue')
-        setTimeout(() => alert("Player 1 claims board no. " + (gridId)), 400)
+        // setTimeout(() => splashWinMini.classList.remove('none'),1000)
+        // nextBoard.classList.add('highlight-blue')
+        scoresListX.innerHTML = gridArray1.length
         gameStart = true
-        console.log(parentElement)  
+        console.log(nextBoard)  
       }    
       // console.log(playerArray1)
     } else {
@@ -220,20 +242,29 @@ function playGame(event, cell) {
       cell.innerHTML === ('O')
       playerArray2[gridId].push(parseInt(cellId))
       
-      console.log(playerArray2)
+      
+      
+      
+      // console.log(playerArray2)
       if (checkIfWin(playerArray2[gridId]) === true) {
         gridArray2.push(parseInt(gridId))
         checkBoardWin(gridArray2) === true
         disabledBoard.push(parseInt(gridId))
         parentElement.classList.add('highlight-red')
-        setTimeout(() => alert("Player 2 claims board no. " + (gridId)), 400)
+        // setTimeout(() => splashWinMini.classList.remove('none'),1000)
+        // nextBoard.classList.add('highlight-red')
+        scoresListO.innerHTML = gridArray2.length
         gameStart = true
-        console.log(parentElement)               
+        console.log(nextBoard)
       }
     }
     
     lastTurn = parseInt(event.target.id)
     
+    // ! This code changes the next grids background to red. You can use a class also, just did this because its quicker.
+    if (!gameStart) {
+    document.querySelector(`.grids-${lastTurn}`).style.backgroundColor = 'white'
+    }
     // console.log(disabledBoard.includes(parseInt(cellId)))
     
     if (!gameStart) {
@@ -253,6 +284,7 @@ function playGame(event, cell) {
   
 
   
+  console.log(event.target.id)
   console.log(event.target.innerHTML)
   console.log(event.target.parentElement)
 }
